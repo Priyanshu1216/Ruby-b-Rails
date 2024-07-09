17ba67b2b7ac6814cf51adb0d69b573ebad56a83
Pri-Nigam <priyanshunigam1607@gmail.com>
2024-07-09 13:08:30 +0530

    each iterator example

---
+++ Assignments/each_iterator_example.rb
@@ -0,0 +1,5 @@
+# Create an array of fruits and use the "each" iterator to iterate over the array and display each fruit name.
+
+arr = ["Mango", "Orange", "Banana", "Pomegranate", "Plum"]
+
+arr.each { |fruit| puts fruit }
